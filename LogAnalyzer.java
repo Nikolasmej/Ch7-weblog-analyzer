1de322df3a46290348d78760d3eb137bb6033907
Nicolas M <43154687+Nikolasmej@users.noreply.github.com>
2018-10-20 18:52:17 -0400

    Exercise 7.16., 7.18, 7.19
    * Exercise 7.16.
         * method quietestHour returns least busy hour.
         * arry hourCounts, for loop.
    Exercise 7.18.
         * Method busiestTwoHour.
         * retuns the value of the first hour of this period.
    Exercise 7.19
         *  objects to analyze daily and monthly web access. need some additions.

--- LogAnalyzer.java
+++ LogAnalyzer.java
@@ -9,6 +9,8 @@ public class LogAnalyzer
 {
     // Where to calculate the hourly access counts.
     private int[] hourCounts;
+    private int[] dayCounts;
+    private int[] monthCounts;
     // Use a LogfileReader to access the data.
     private LogfileReader reader;
 
@@ -18,17 +20,23 @@ public class LogAnalyzer
      * 
      * parameter to accept log file name as input.
      * @String fileName
-     *  
+     *  Exercise 7.19
+     *  objects to analyze daily and monthly web access.
      */
     public LogAnalyzer(String fileName)
     { 
-        // Create the array object to hold the hourly
+        // Create the array object to hold the hourly, monthly, and daily.
         // access counts.
         hourCounts = new int[24];
+        dayCounts = new int[365];
+        monthCounts = new int[12];
         // Create the reader to obtain the data.
         reader = new LogfileReader(fileName);
     }
-
+    
+    
+    
+    
     /**
      * Analyze the hourly access data from the log file.
      */
@@ -42,7 +50,31 @@ public class LogAnalyzer
     }
 
     /**
-     * Print the hourly counts.
+     * Analyze the daily access data from the log file. EX :7.19
+     */
+    public void analyzeDailyData()
+    {
+        while(reader.hasNext()) {
+            LogEntry entry = reader.next();
+            int day = entry.getDay();
+            dayCounts[day]++;
+        }
+    }
+    /**
+     * Analyze the daily access data from the log file. EX :7.19
+     */
+    public void analyzeMonthlyData()
+    {
+        while(reader.hasNext()) {
+            LogEntry entry = reader.next();
+            int month = entry.getMonth();
+            monthCounts[month]++;
+        }
+    }
+    
+    
+    /**
+     * Print the hourly counts. 
      * These should have been set with a prior
      * call to analyzeHourlyData.
      */
@@ -51,6 +83,32 @@ public class LogAnalyzer
         System.out.println("Hr: Count");
         for(int hour = 0; hour < hourCounts.length; hour++) {
             System.out.println(hour + ": " + hourCounts[hour]);
+        }
+    }
+    
+    /**
+     * Print the daily  counts. Ex 7.19
+     * These should have been set with a prior
+     * call to analyzeDailyData.
+     */
+    public void printDailyCounts()
+    {
+        System.out.println("Day: Count");
+        for(int day = 0; day < dayCounts.length; day++) {
+            System.out.println(day + ": " + dayCounts[day]);
+        }
+    }
+    
+    /**
+     * Print the daily  counts. Ex 7.19
+     * These should have been set with a prior
+     * call to analyzeMonthlyData.
+     */
+    public void printMonthlyCounts()
+    {
+        System.out.println("Month: Count");
+        for(int month = 0; month < monthCounts.length; month++) {
+            System.out.println(month + ": " + monthCounts[month]);
         }
     }
     
@@ -64,7 +122,7 @@ public class LogAnalyzer
 
      /**
      * Exrecise 7.14.
-     * Method to Check the number of accesses
+     * Method to Check the number of hourly accesses
      * online and book references.
      */ 
      public int numberOfAccesses()
@@ -74,10 +132,27 @@ public class LogAnalyzer
          total += hourCounts [hour];
         }
      return total;
+     
+     
+     
      }
     
+     /**
+     * Exrecise 7.19.
+     * Method to Check the number ofdaily accesses
+     * online and book references.
+     */ 
+     public int numberOfAccessesPerMonth()
+     {
+     int total = 0;
+     for (int month = 0; month < monthCounts.length; month++){
+         total += monthCounts [month];
+        }
+     return total;
+     }
+     
     /**
-     * Exrecise 7.15.
+     * Exercise 7.15.
      * Method busiestHour returns busiest hour.
      * online and book references.
      * comparing values of hoursCount to hour mx
@@ -94,9 +169,52 @@ public class LogAnalyzer
         }
     }
     return maxHour;
-   }
+    }
          
-         
+    /**
+     *Exercise 7.16.
+     * method quietestHour returns least busy hour. 
+     * arry hourCounts, for loop.
+     * online and book references.
+     */ 
+     public int quietestHour(int [] hourCounts)
+     {
+     int quietest = hourCounts[0];
+     for ( int i=0; i< hourCounts.length; i++)
+     {
+         if (hourCounts [i] > 0 &&
+         hourCounts[i] > quietest)
+         quietest = hourCounts[i];
+        }
+        return quietest;
+    }
+
+    /**
+     * Exercise 7.18.
+     * Method busiestTwoHour. 
+     * retuns the value of the first hour of this period.
+     * online and book references.
+     */ 
+      int busiestTwoHour (int [] hourCounts)
+     {
+     int busiestOne = 0;
+     int busiestTwo = 0;
+     for ( int n :hourCounts) {
+         if(busiestOne < n){
+             busiestTwo =  busiestOne;
+             busiestOne = n;
+            }
+            else if (busiestTwo < n){
+                busiestTwo = n;
+            }
+        }
+        return busiestOne;
+     }
+    
+    
+    
+    
+}     
 
      
- }
+ 
